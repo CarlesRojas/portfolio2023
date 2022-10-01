2016d5687250ed0a4f997cad9d3a224741ef730e
CarlesRojas <carles.rojas@mundimoto.com>
2022-10-01 08:22:30 +0200

    detect device

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,20 +1,28 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, Variant, Variants } from "framer-motion";
 import { useRouter } from "next/router";
 import s from "@styles/components/Transition.module.scss";
 import { RoutePaths } from "@interfaces/routes";
+import { isMobile } from "react-device-detect";
+import { useEffect, useState } from "react";
 
 type TransitionProps = { children: JSX.Element };
 
-const opacityVariant = {
+const opacityVariant: Variants = {
     out: { opacity: 0, y: 0, transition: { duration: 0.2, ease: "easeInOut" } },
     inactive: { opacity: 1, y: 0, transition: { duration: 0.2, ease: "easeInOut" } },
     in: { opacity: 0, y: 0, transition: { duration: 0.2, ease: "easeInOut" } },
 };
 
-const translationVariant = {
+const translationVariant: Variants = {
     out: { opacity: 0, y: "25vh", transition: { duration: 0.2, ease: "easeInOut" } },
     inactive: { opacity: 1, y: 0, transition: { duration: 0.2, ease: "easeInOut" } },
     in: { opacity: 0, y: "25vh", transition: { duration: 0.2, ease: "easeInOut" } },
+};
+
+const mobileVariant: Variants = {
+    out: { opacity: 1 },
+    inactive: { opacity: 1 },
+    in: { opacity: 1 },
 };
 
 const opacityVariants: string[] = [RoutePaths.HOME, RoutePaths.PRIVATE_HOME, RoutePaths.LOGIN];
@@ -23,7 +31,12 @@ const Transition = ({ children }: TransitionProps) => {
     const router = useRouter();
     const { asPath } = router;
 
-    const variant = opacityVariants.includes(asPath) ? opacityVariant : translationVariant;
+    const [variant, setVariant] = useState(opacityVariants.includes(asPath) ? opacityVariant : translationVariant);
+
+    useEffect(() => {
+        console.log(isMobile);
+        if (isMobile) setVariant(mobileVariant);
+    }, []);
 
     return (
         <AnimatePresence mode="wait">
